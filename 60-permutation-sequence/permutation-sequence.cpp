4326f4475f564c540be09dc125667907451679a8
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-01-24 18:51:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (68.70%) - LeetSync

---
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        vector<int> nums;
+        int fact = 1;
+        for(int i = 1; i<n; i++) {
+            fact = fact * i;
+            nums.push_back(i);
+        }
+        nums.push_back(n);
+        string res = "";
+        k = k-1;
+        while(1) {
+            res += to_string(nums[k/fact]);
+            nums.erase(nums.begin() + k/fact);
+            if(nums.size() == 0) break;
+            k = k%fact;
+            fact = fact / nums.size();
+        }
+        return res;
+    }
+};
