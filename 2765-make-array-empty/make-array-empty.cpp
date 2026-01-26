7ec1c1a08b802387241826a9a2b47ca2939f3bdf
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-01-26 12:08:47 +0530

    Time: 32 ms (87.72%) | Memory: 85.1 MB (59.65%) - LeetSync

---
+++ 2765-make-array-empty/make-array-empty.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long countOperationsToEmptyArray(vector<int>& nums) {
+        vector<pair<int,int>> pos;
+        for(int i = 0; i<nums.size(); i++) {
+            pos.push_back({nums[i],i});
+        }
+        sort(pos.begin(),pos.end());
+        int n = nums.size();
+        long long op = n;
+        for(int i = 1; i<n; i++) {
+            if(pos[i].second < pos[i-1].second) {
+                op += n - i;
+            }
+        }
+        return op;
+    }
+};
