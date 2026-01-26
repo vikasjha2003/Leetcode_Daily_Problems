de51f6bb2c8768c8a40e5a0257349b9887b792ee
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-01-26 10:01:46 +0530

    Time: 16 ms (47.69%) | Memory: 36.7 MB (64.03%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
@@ -6,11 +6,14 @@ public:
         int mina = INT_MAX;
         for(int i = 0; i<arr.size()-1; i++) {
             int a = abs(arr[i] - arr[i+1]);
-            mina = min(mina,a);
-        }
-        for(int i = 0; i<arr.size()-1; i++) {
-            int a = abs(arr[i] - arr[i+1]);
-            if(a == mina) pairs.push_back({arr[i],arr[i+1]});
+            if(a < mina) {
+                mina = a;
+                pairs.clear();
+                pairs.push_back({arr[i],arr[i+1]});
+            }
+            else if(mina == a) {
+                pairs.push_back({arr[i],arr[i+1]});
+            }
         }
         return pairs;
     }
