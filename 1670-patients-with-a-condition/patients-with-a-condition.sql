ea7bfd0be9e59f0baa609b63d301d4940b7e4ab3
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-01-30 19:22:41 +0530

    Time: 398 ms (37.01%) | Memory: 0B (100.00%) - LeetSync

--- 1670-patients-with-a-condition/patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/patients-with-a-condition.sql
@@ -1,6 +1,2 @@
-# Write your MySQL query statement below
-select * from Patients
-where
-conditions  like "% DIAB1%"
-or conditions like "DIAB1%"
-;
+SELECT * FROM Patients
+WHERE  conditions LIKE 'DIAB1%' OR conditions LIKE '% DIAB1%';
