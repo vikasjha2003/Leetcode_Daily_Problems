97cb6525fb74649be6aef9eb80eb71362a56a71b
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-01-30 23:55:05 +0530

    Time: 4 ms (85.04%) | Memory: 29.5 MB (62.91%) - LeetSync

--- 436-find-right-interval/find-right-interval.cpp
+++ 436-find-right-interval/find-right-interval.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     vector<int> findRightInterval(vector<vector<int>>& intervals) {
         int n = intervals.size();
-        unordered_map<int,int> pos;
+        vector<pair<int,int>> pos;
         for(int i = 0; i<n; i++) {
-            pos[intervals[i][0]] = i;
+            pos.push_back({intervals[i][0],i});
         }
         vector<int> res (n,-1);
-        sort(intervals.begin(),intervals.end());
+        sort(pos.begin(),pos.end());
         for(int i = 0; i<n; i++) {
             int target = intervals[i][1];
-            int lo = i, hi = n-1;
+            int lo = 0, hi = n-1;
             while(lo<=hi) {
                 int mid = lo + (hi - lo)/2;
-                if(intervals[mid][0] >= target) {
+                if(pos[mid].first >= target) {
                     hi = mid - 1;
                 } else {
                     lo = mid + 1;
                 }
             }
-            if (lo < n && lo >= i) res[pos[intervals[i][0]]] = pos[intervals[lo][0]];
+            if (lo < n) res[i] = pos[lo].second;
         }
         return res;
     }
