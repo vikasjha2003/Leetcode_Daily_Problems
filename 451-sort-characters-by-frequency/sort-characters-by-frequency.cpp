acc5a977df9e95dbbf16b4aeb995d4e3a8915e25
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-01-31 00:28:44 +0530

    Time: 1 ms (68.17%) | Memory: 11.7 MB (20.96%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -5,13 +5,8 @@ public:
     }
     string frequencySort(string s) {
         unordered_map<char,int> freq;
-        for(int i = 0; i<s.length(); i++) {
-            freq[s[i]]++;
-        }
-        vector<pair<char,int>> ch;
-        for(auto & q : freq) {
-            ch.push_back(q);
-        }
+        for(char c : s) freq[c]++;
+        vector<pair<char,int>> ch (freq.begin(),freq.end());
         sort(ch.begin(),ch.end(),cmp);
         string res = "";
         for(int i = 0; i<ch.size(); i++) {
