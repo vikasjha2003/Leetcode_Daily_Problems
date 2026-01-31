27da57d7fb49cc7e7b4817996b4978bb7d8fc955
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-01-31 17:27:44 +0530

    Time: 111 ms (62.64%) | Memory: 11.2 MB (49.88%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -5,14 +5,15 @@ public:
         int res = 0;
         for(int i = 0; i<n; i++) {
             vector<int> freq (26,0);
-            int maxi = 0;
             for(int j = i; j<n; j++) {
                 freq[s[j] - 'a']++;
-                int mini = INT_MAX;
+                int mini = INT_MAX , maxi = 0;
                 for(int k = 0; k<26; k++) {
-                    if(freq[k] != 0) mini = min(mini,freq[k]);
+                    if(freq[k] != 0) {
+                        mini = min(mini,freq[k]);
+                        maxi = max(maxi,freq[k]);
+                    }
                 }
-                maxi = max(maxi,freq[s[j] - 'a']);
                 res += maxi - mini;
             }
         }
