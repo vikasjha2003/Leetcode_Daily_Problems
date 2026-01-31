657bb53d448991af4bd96810e3cdfa2a2a087152
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-01-31 22:32:06 +0530

    Time: 98 ms (66.82%) | Memory: 28.2 MB (58.39%) - LeetSync

---
+++ 454-4sum-ii/4sum-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        unordered_map<int,int> freq;
+        for(int i : nums4) {
+            for(int j : nums3) {
+                freq[i + j]++;
+            }
+        }
+        int count = 0;
+        for(int &i : nums1) {
+            for(int &j : nums2) {
+                if(freq.find(0-i-j) != freq.end()) {
+                    count += freq[0-i-j];
+                }
+            }
+        }
+        return count;
+    }
+};
