9b4066a01f3d6a20819abf16b53568c4a9b9f051
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-01-31 10:14:26 +0530

    Time: 4 ms (16.93%) | Memory: 15.8 MB (14.64%) - LeetSync

--- 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.cpp
@@ -3,9 +3,7 @@ public:
     int minMoves2(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(),nums.end());
-        int median;
-        if(n % 2 == 1) median = nums[n/2];
-        else median = nums[(n/2)-1];
+        int median = nums[n/2];
         int res = 0;
         for(int i = 0; i<n; i++) {
             res += abs(median - nums[i]);
