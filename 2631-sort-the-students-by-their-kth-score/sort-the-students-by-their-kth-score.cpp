57603bae521931cd9f42df82c46847b1305e5dda
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-02-03 22:55:39 +0530

    Time: 0 ms (100.00%) | Memory: 32.2 MB (44.05%) - LeetSync

--- 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.cpp
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
-        sort(score.begin(),score.end(),[k](vector<int> &v1 , vector<int> &v2) {
+        sort(score.begin(),score.end(),[k](const vector<int> &v1 ,const  vector<int> &v2) {
             return v1[k] > v2[k];
         });
         return score;
