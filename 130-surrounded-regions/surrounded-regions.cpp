6295793adb1d36a6f461c18f79600756665846dc
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-02-04 11:16:03 +0530

    Time: 3 ms (25.04%) | Memory: 21.8 MB (5.21%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-
+    int m , n;
     void DFS(vector<pair<int,int>>& collection , vector<vector<char>>& board , vector<vector<bool>> &visited, bool &flag, int row , int col) {
 
-        if(row < 0 || col < 0 || row == board.size() || col == board[0].size() 
-        || visited[row][col] == true || board[row][col] == 'X') return;
+        if(row < 0 || col < 0 || row == m || col == n || visited[row][col] == true || board[row][col] == 'X') return;
 
         visited[row][col] = true;
         collection.push_back({row,col});
 
-        if(row + 1 == board.size() || col + 1 == board[0].size() 
-        || row - 1 < 0 || col - 1 < 0) flag = true;
+        if(row + 1 == m || col + 1 == n || row - 1 < 0 || col - 1 < 0) flag = true;
 
         DFS(collection,board,visited,flag,row + 1,col);
         DFS(collection,board,visited,flag,row - 1,col);
@@ -19,9 +17,8 @@ public:
     }
 
     void solve(vector<vector<char>>& board) {
-        int m = board.size();
-        int n = board[0].size();
-        vector<vector<char>> res (m,vector<char> (n,'X'));
+        m = board.size();
+        n = board[0].size();
         vector<vector<bool>> visited (m,vector<bool> (n,false));
         for(int i = 0; i<m; i++) {
             for(int j = 0; j<n; j++) {
