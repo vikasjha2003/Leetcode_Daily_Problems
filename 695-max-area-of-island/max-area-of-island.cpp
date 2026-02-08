d885d896c2cc7138e3db66834f38ed5e584f2e5b
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-02-08 23:21:21 +0530

    Time: 5 ms (29.63%) | Memory: 31.6 MB (17.73%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -1,24 +1,34 @@
 class Solution {
 public:
     int m , n;
-    int DFS (vector<vector<int>>& grid , vector<vector<bool>>& visited , int row , int col) {
-        if(row < 0 || col < 0 || row == m || col == n || visited[row][col] == true || grid[row][col] == 0) return 0;
-        visited[row][col] = true;
-        int a = DFS(grid,visited,row+1,col);
-        int b = DFS(grid,visited,row-1,col);
-        int c = DFS(grid,visited,row,col+1);
-        int d = DFS(grid,visited,row,col-1);
-        return 1 + a + b + c + d;
-    }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         m = grid.size();
         n = grid[0].size();
         vector<vector<bool>> visited (m , vector<bool> (n,false));
         int maxi = 0;
+        int drow[] = {0,1,0,-1};
+        int dcol[] = {1,0,-1,0}; 
         for(int i = 0; i<m; i++) {
             for(int j = 0; j<n; j++) {
-                int a = DFS(grid,visited,i,j);
-                if(maxi < a) maxi = a;
+                if(grid[i][j] == 1 && !visited[i][j]) {
+                    queue<pair<int,int>> q;
+                    q.push({i,j});
+                    visited[i][j] = true;
+                    int sum = 0;
+                    while(!q.empty()) {
+                        auto it = q.front();
+                        q.pop();
+                        sum += grid[it.first][it.second];
+                        for(int k = 0; k<4; k++) {
+                            int row = it.first + drow[k];
+                            int col = it.second + dcol[k];
+                            if(row < 0 || col < 0 || row == m || col == n || visited[row][col] == true || grid[row][col] == 0) continue;
+                            q.push({row,col});
+                            visited[row][col] = true;
+                        }
+                    }
+                    maxi = max(sum,maxi);
+                }
             }
         }
         return maxi;
