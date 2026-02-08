562558c24c13bb24a9865cf27317bc604ed67eff
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-02-08 16:25:40 +0530

    Time: 13 ms (77.02%) | Memory: 199 MB (88.25%) - LeetSync

--- 4213-merge-adjacent-equal-elements/merge-adjacent-equal-elements.cpp
+++ 4213-merge-adjacent-equal-elements/merge-adjacent-equal-elements.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     vector<long long> mergeAdjacent(vector<int>& nums) {
-        vector<long long> res;
-        res.push_back(nums[0]);
-        for(int i = 1; i<nums.size(); i++) {
+        vector<long long> stk; 
+        for(int i = 0; i<nums.size(); i++) {
             long long a = nums[i];
-            while(res.size() != 0 && res.back() == a) {
-                a += res.back();
-                res.pop_back();
+            while(stk.size() != 0 && stk.back() == a) {
+                a += stk.back();
+                stk.pop_back();
             }
-            res.push_back(a);
+            stk.push_back(a);
         }
-        return res;
+        return stk;
     }
 };
