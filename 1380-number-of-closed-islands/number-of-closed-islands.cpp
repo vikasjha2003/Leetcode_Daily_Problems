143ca1e1563fa16d765052e38cdcfcc1e911fc80
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-02-09 19:11:30 +0530

    Time: 15 ms (9.20%) | Memory: 19.3 MB (6.64%) - LeetSync

--- 1380-number-of-closed-islands/number-of-closed-islands.cpp
+++ 1380-number-of-closed-islands/number-of-closed-islands.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    int m , n;
-    void DFS(vector<vector<int>> &grid, vector<vector<bool>> &visited, int row, int col, bool &flag) {
-        if(row < 0 || col < 0 || row == m || col == n || visited[row][col] == true || grid[row][col] == 1) return;
-        visited[row][col] = true;
-        if(row -1 < 0 || col - 1 < 0 || row + 1 == m || col + 1 == n) flag = true;
-        DFS(grid , visited , row + 1, col, flag);
-        DFS(grid , visited , row - 1, col, flag);
-        DFS(grid , visited , row, col + 1, flag);
-        DFS(grid , visited , row, col - 1, flag);
-    }
     int closedIsland(vector<vector<int>>& grid) {
-        m = grid.size();
-        n = grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
+        int drow[] = {1,0,-1,0};
+        int dcol[] = {0,1,0,-1};
         int count = 0;
         vector<vector<bool>> visited (m, vector<bool> (n,false));
         for(int i = 0; i<m; i++) {
             for(int j = 0; j<n; j++) {
                 if(grid[i][j] == 0 && visited[i][j] == false) {
                     bool flag = false;
-                    DFS(grid,visited,i,j,flag);
+                    queue<pair<int,int>> q;
+                    q.push({i,j});
+                    while(!q.empty()) {
+                        auto &it = q.front();
+                        visited[it.first][it.second] = true;
+                        if(it.first -1 < 0 || it.second - 1 < 0 || it.first + 1 == m || it.second + 1 == n) flag = true;
+                        for(int k = 0; k < 4; k++) {
+                            int row = it.first + drow[k];
+                            int col = it.second + dcol[k];
+                            if(row < 0 || col < 0 || row == m || col == n || visited[row][col] == true || grid[row][col] == 1) continue;
+                            q.push({row,col});
+                        }
+                        q.pop();
+                    }
                     if(!flag) count++;
                 }
             }
