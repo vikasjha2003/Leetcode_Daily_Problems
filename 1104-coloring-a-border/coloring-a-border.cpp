af1e375b2e4d2abe7f0326fc43973675fcef4311
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-02-10 12:44:45 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (63.96%) - LeetSync

--- 1104-coloring-a-border/coloring-a-border.cpp
+++ 1104-coloring-a-border/coloring-a-border.cpp
@@ -1,15 +1,34 @@
 class Solution {
 public:
     int m , n;
-    void DFS (const vector<vector<int>> &grid, vector<vector<int>> &res, vector<vector<bool>> &visited, int row , int col , const int &color, const int &component) {
-        if(row < 0 || col < 0 || row == m || col == n || visited[row][col] || grid[row][col] != component) return;
+    void DFS(const vector<vector<int>> &grid,
+            vector<vector<int>> &res,
+            vector<vector<bool>> &visited,
+            int row, int col,
+            const int &color,
+            const int &component) {
+
+        if(row < 0 || col < 0 || row == m || col == n ||
+        visited[row][col] || grid[row][col] != component)
+            return;
+
         visited[row][col] = true;
-        if(row == 0 || col == 0 || row == m-1 || col == n-1 || grid[row+1][col] != component || grid[row-1][col] != component || grid[row][col+1] != component || grid[row][col-1] != component) res[row][col] = color; // boundary and border
-        DFS(grid,res,visited,row+1,col,color,component);
-        DFS(grid,res,visited,row-1,col,color,component);
-        DFS(grid,res,visited,row,col+1,color,component);
-        DFS(grid,res,visited,row,col-1,color,component);
+
+        bool isBorder = false;
+        int drow[] = {1, -1, 0, 0};
+        int dcol[] = {0, 0, 1, -1};
+
+        for(int d = 0; d < 4; d++) {
+            int nr = row + drow[d];
+            int nc = col + dcol[d];
+            if(nr < 0 || nc < 0 || nr >= m || nc >= n || grid[nr][nc] != component) {
+                isBorder = true;
+            }
+            DFS(grid, res, visited, nr, nc, color, component);
+        }
+        if(isBorder) res[row][col] = color;
     }
+
     vector<vector<int>> colorBorder(vector<vector<int>>& grid, int row, int col, int color) {
         m = grid.size();
         n = grid[0].size();
