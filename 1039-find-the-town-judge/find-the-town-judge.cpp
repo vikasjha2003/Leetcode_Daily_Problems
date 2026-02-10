67bfae1307c5c737bfaa5defd0b8bf7642a59152
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-02-10 16:01:24 +0530

    Time: 0 ms (100.00%) | Memory: 65.1 MB (64.31%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
         vector<int> istrusted (n+1,0);
-        unordered_set<int> st;
-        for(auto it : trust) {
-            st.insert(it[0]);
+        vector<bool> trusts (n+1,false);
+        for(auto &it : trust) {
+            trusts[it[0]] = true;
             istrusted[it[1]]++;
         }
         for(int i = 1; i<n+1; i++) {
-            if(istrusted[i] == n-1 && st.find(i) == st.end()) return i;
+            if(istrusted[i] == n-1 && trusts[i] == false) return i;
         }
         return -1;
     }
