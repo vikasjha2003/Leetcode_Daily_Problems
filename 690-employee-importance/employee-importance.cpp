3b61701fa9898b3d4fc5fd9a8d124f9485ca85a4
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-02-10 15:30:21 +0530

    Time: 19 ms (91.09%) | Memory: 21.8 MB (32.01%) - LeetSync

--- 690-employee-importance/employee-importance.cpp
+++ 690-employee-importance/employee-importance.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    int DFS (unordered_map<int,pair<int,vector<int>>> &mpp, int node) {
-        auto it = mpp[node];
-        int sum = 0;
-        for(int i : it.second) {
-            sum += DFS(mpp,i);
-        }
-        return it.first + sum;
-    }
     int getImportance(vector<Employee*> employees, int id) {
         unordered_map<int,pair<int,vector<int>>> mpp;
         for(auto it : employees) {
             mpp[it->id] = {it->importance,it->subordinates};
         }
-        return DFS(mpp,id);
+        queue<int> q;
+        int res = 0;
+        q.push(id);
+        while(!q.empty()) {
+            auto it = mpp[q.front()];
+            q.pop();
+            res += it.first;
+            for(int i : it.second) {
+                q.push(i);
+            }
+        }
+        return res;
     }
 };
