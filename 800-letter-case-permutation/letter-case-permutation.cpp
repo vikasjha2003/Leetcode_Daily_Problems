f9097a59e91a899834da56ae5e52fbbd99644383
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-02-11 16:23:05 +0530

    Time: 3 ms (58.75%) | Memory: 14.5 MB (59.02%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.cpp
+++ 800-letter-case-permutation/letter-case-permutation.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
+
     void generate(string &s, string ans , vector<string> &res, int idx) {
         if(idx == s.length()) {
             res.push_back(ans);
             return;
         }
-        generate(s,ans + s[idx],res,idx+1); // will always be called irrespective of the character
+        generate(s,ans + s[idx],res,idx+1);
         if(s[idx] >= 'A' && s[idx] <= 'Z') {
             char c = s[idx] + 32;
             generate(s,ans + c,res,idx+1);
@@ -15,6 +16,7 @@ public:
             generate(s,ans + c,res,idx+1);
         }
     }
+
     vector<string> letterCasePermutation(string s) {
         vector<string> res;
         generate(s,"",res,0);
