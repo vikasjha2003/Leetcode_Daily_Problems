57ddcef50e4f6979196e3f5004d05957f52ae542
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-02-12 19:36:38 +0530

    Time: 0 ms (100.00%) | Memory: 31.8 MB (34.32%) - LeetSync

--- 2662-check-knight-tour-configuration/check-knight-tour-configuration.cpp
+++ 2662-check-knight-tour-configuration/check-knight-tour-configuration.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    // declared here to avoid passing as parameters
-    int n;
-    int drow[8] = {2,2,1,1,-1,-1,-2,-2};
-    int dcol[8] = {1,-1,2,-2,2,-2,1,-1};
-
-    bool res (vector<vector<int>>& grid, int row , int col, int move) {
-        if(move == n*n - 1) return true;
-        for(int i = 0; i<8; i++) {
-            int nr = row + drow[i];
-            int nc = col + dcol[i];
-            if(nr < 0 || nc < 0 || nr >= n || nc >= n || grid[nr][nc] != move + 1) continue;
-            return res(grid,nr,nc,move+1);
-        }
-        return false;
-    }
     bool checkValidGrid(vector<vector<int>>& grid) {
         if(grid[0][0] != 0) return false;
-        n = grid.size();
-        return res(grid,0,0,0);
+        int n = grid.size();
+        int drow[8] = {2,2,1,1,-1,-1,-2,-2};
+        int dcol[8] = {1,-1,2,-2,2,-2,1,-1};
+        queue<pair<int,int>> q;
+        q.push({0,0});
+        for(int i = 1; i< n*n; i++) {
+            auto it = q.front();
+            q.pop();
+            for(int j = 0; j<8; j++) {
+                int row = it.first + drow[j];
+                int col = it.second + dcol[j];
+                if(row < 0 || col < 0 || row >= n || col >= n || grid[row][col] != i) continue;
+                q.push({row,col});
+                break;
+            }
+            if(q.empty()) return false;
+        }
+        return true;  
     }
 };
