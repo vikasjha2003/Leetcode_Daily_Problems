6da7ac4cfe3982e85408a50bc8c805ab694cb11f
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-02-14 12:31:36 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (68.03%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -14,7 +14,7 @@ public:
     bool canFinish(int n, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj (n);
         for(auto &it : prerequisites) {
-            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
         }
         vector<bool> visited (n,false);
         vector<bool> pathvisited (n,false);
