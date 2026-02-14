c3893dbb2957f6e00509d1443b02607708011c4d
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-02-14 13:02:04 +0530

    Time: 5 ms (44.36%) | Memory: 18.5 MB (52.96%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     bool DFS(vector<vector<int>> &adj, vector<bool> &visited, 
-    vector<bool> &pathvisited, int node, stack<int> &st) {
+    vector<bool> &pathvisited, int node, vector<int> &res) {
             pathvisited[node] = visited[node] = true;
             for(int &i : adj[node]) {
                 if(pathvisited[i]) return true;
                 if(!visited[i]) {
-                    if(DFS(adj,visited,pathvisited,i,st)) return true;
+                    if(DFS(adj,visited,pathvisited,i,res)) return true;
                 }
             }
-            st.push(node);
+            res.push_back(node);
             pathvisited[node] = false;
             return false;
         }
@@ -20,17 +20,13 @@ public:
         }
         vector<bool> visited (n,false);
         vector<bool> pathvisited (n,false);
-        stack<int> st;
+        vector<int> res;
         for(int start = 0; start<n; start++) {
             if(!visited[start]) {
-                if(DFS(adj,visited,pathvisited,start,st)) return {};
+                if(DFS(adj,visited,pathvisited,start,res)) return {};
             }
         }
-        vector<int> res;
-        while(!st.empty()) {
-            res.push_back(st.top());
-            st.pop();
-        }
+        reverse(res.begin(),res.end());
         return res;
     }
 };
