0ed2c5b6253433ce6b0f5ea81c3708a5d9c8c75a
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-02-14 12:06:39 +0530

    Time: 7 ms (48.22%) | Memory: 20.1 MB (36.21%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+        vector<int> indegree (n,0);
+        vector<vector<int>> adj (n);
+        for(auto it : prerequisites) {
+            adj[it[0]].push_back(it[1]);
+            indegree[it[1]]++;
+        }
+        queue<int> q;
+        for(int i = 0; i<n ; i++) {
+            if(indegree[i] == 0) q.push(i);
+        }
+        int count = 0;
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            count++;
+            for(int i : adj[node]) {
+                indegree[i]--;
+                if(indegree[i] == 0) q.push(i);
+            }
+        }
+        if(count == n) return true;
+        return false;
+    }
+};
