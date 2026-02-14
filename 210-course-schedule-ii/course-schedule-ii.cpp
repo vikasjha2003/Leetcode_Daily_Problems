ff2e3a71688847eac22cc9faba1293a2e7031a37
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-02-14 12:50:20 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (94.48%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -7,16 +7,18 @@ public:
             adj[i[1]].push_back(i[0]);
             indegree[i[0]]++;
         }
-        vector<int> res;
+        queue<int> q;
         for(int i = 0; i<n; i++) {
-            if(indegree[i] == 0) res.push_back(i);
+            if(indegree[i] == 0) q.push(i);
         }
-        
-        for(int st = 0 ; st<res.size(); st++) {
-            int node = res[st];
+        vector<int> res;
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            res.push_back(node);
             for(int &i : adj[node]) {
                 indegree[i]--;
-                if(indegree[i] == 0) res.push_back(i);
+                if(indegree[i] == 0) q.push(i);
             }
         }
         if(res.size() == n) return res;
