1164fafcca3b59f49384310b83c58e4cf740b19b
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-02-14 12:58:49 +0530

    Time: 7 ms (34.67%) | Memory: 18.4 MB (57.93%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
+    bool DFS(vector<vector<int>> &adj, vector<bool> &visited, 
+    vector<bool> &pathvisited, int node, stack<int> &st) {
+            pathvisited[node] = visited[node] = true;
+            for(int &i : adj[node]) {
+                if(pathvisited[i]) return true;
+                if(!visited[i]) {
+                    if(DFS(adj,visited,pathvisited,i,st)) return true;
+                }
+            }
+            st.push(node);
+            pathvisited[node] = false;
+            return false;
+        }
     vector<int> findOrder(int n, vector<vector<int>>& graph) {
-        vector<int> indegree (n,0);
         vector<vector<int>> adj (n);
         for(auto &i : graph) {
             adj[i[1]].push_back(i[0]);
-            indegree[i[0]]++;
         }
-        queue<int> q;
-        for(int i = 0; i<n; i++) {
-            if(indegree[i] == 0) q.push(i);
-        }
-        vector<int> res;
-        while(!q.empty()) {
-            int node = q.front();
-            q.pop();
-            res.push_back(node);
-            for(int &i : adj[node]) {
-                indegree[i]--;
-                if(indegree[i] == 0) q.push(i);
+        vector<bool> visited (n,false);
+        vector<bool> pathvisited (n,false);
+        stack<int> st;
+        for(int start = 0; start<n; start++) {
+            if(!visited[start]) {
+                if(DFS(adj,visited,pathvisited,start,st)) return {};
             }
         }
-        if(res.size() == n) return res;
-        return {};
+        vector<int> res;
+        while(!st.empty()) {
+            res.push_back(st.top());
+            st.pop();
+        }
+        return res;
     }
 };
