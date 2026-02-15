f7a98403f05fae1df5266e30169f490c0f7832b7
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-02-15 15:27:05 +0530

    Time: 47 ms (23.08%) | Memory: 110 MB (7.69%) - LeetSync

--- 4137-number-of-prefix-connected-groups/number-of-prefix-connected-groups.cpp
+++ 4137-number-of-prefix-connected-groups/number-of-prefix-connected-groups.cpp
@@ -4,11 +4,11 @@ public:
         int max_count = 0;
         unordered_map<string,int> mpp;
         for(int i = 0; i<words.size(); i++) {
-            if(words[i].length() >= k) {
-                string key = words[i].substr(0,k);
-                mpp[key]++;
-                if(mpp[key] == 2) max_count++;
-            }                
+            if(words[i].length() >= k) 
+                mpp[words[i].substr(0,k)]++;
+        }
+        for(auto &it : mpp) {
+            if(it.second >= 2) max_count++;
         }
         return max_count;
     }
