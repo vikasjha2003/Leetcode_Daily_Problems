b19ee7bcec18a2ef0028273d21dd07b351f3df91
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-02-15 17:14:30 +0530

    Time: 186 ms (37.47%) | Memory: 289.6 MB (67.37%) - LeetSync

---
+++ 4217-first-element-with-unique-frequency/first-element-with-unique-frequency.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int firstUniqueFreq(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int> freq;
+        unordered_map<int,int> freq_count;
+        for(int i = 0; i<n; i++) {
+            freq[nums[i]]++;
+        }
+        for(auto &it : freq) {
+            freq_count[it.second]++;
+        }
+        for(int i = 0; i<n; i++) {
+            if(freq_count[freq[nums[i]]] == 1) return nums[i];
+        }
+        return -1;
+    }
+};
