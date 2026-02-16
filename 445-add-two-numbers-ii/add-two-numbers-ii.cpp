d8236ae4fe49b4029e13bb2ea027ea3df299c6f1
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-02-16 13:42:16 +0530

    Time: 4 ms (35.25%) | Memory: 79.5 MB (21.56%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/add-two-numbers-ii.cpp
@@ -1,12 +1,5 @@
 class Solution {
 public:
-    ListNode* rev (ListNode* head) {
-        if(head->next == NULL) return head;
-        ListNode* temp = rev(head -> next);
-        head -> next -> next = head;
-        head -> next = NULL;
-        return temp;
-    }
     ListNode* add(ListNode* l1, ListNode* l2) {
         ListNode* temp1 = l1;
         ListNode* temp2 = l2;
@@ -30,9 +23,36 @@ public:
         return dummy -> next;
     }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* h1 = rev(l1);
-        ListNode* h2 = rev(l2);
-        ListNode* res = add(h1,h2);
-        return rev(res);
+        ListNode* h1 = l1;
+        ListNode* h2 = l2;
+        stack<int> st1;
+        stack<int> st2;
+        while(h1 || h2) {
+            if(h1) {
+                st1.push(h1->val);
+                h1 = h1->next;
+            }
+            if(h2) {
+                st2.push(h2->val);
+                h2 = h2 -> next;
+            }
+        }
+        int carry = 0;
+        ListNode* head = NULL;
+        while(!st1.empty() || !st2.empty() || carry) {
+            if(!st1.empty()) {
+                carry += st1.top();
+                st1.pop();
+            }
+            if(!st2.empty()) {
+                carry += st2.top();
+                st2.pop();
+            }
+            ListNode* Node = new ListNode(carry%10);
+            Node -> next = head;
+            head = Node;
+            carry /= 10;
+        }
+        return head;
     }
 };
