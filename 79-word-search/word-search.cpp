eeb55ba6b20d2ff009aa4c44d198adda84d57445
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-02-18 15:05:25 +0530

    Time: 247 ms (59.31%) | Memory: 10.6 MB (69.51%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    int n , m;
+    int n , m, w;
     int drow[4] = {0,0,1,-1};
     int dcol[4] = {1,-1,0,0}; 
     bool DFS (vector<vector<char>>& board, vector<vector<bool>> &visited, 
     string &word, int idx, int row , int col) {
-        if(idx == word.size()-1) return true;
+        if(idx == w-1) return true;
         visited[row][col] = true;
-        for(int i = 0; i<4; i++) {
+        for(int i = 0; i<4; i++) {         
             int nr = row + drow[i];
             int nc = col + dcol[i];
-            if(nr < 0 || nc < 0 || nr == m || nc == n || 
-            visited[nr][nc] == true || board[nr][nc] != word[idx+1]) 
-                continue;
+            if(nr < 0 || nc < 0 || nr == m || nc == n || visited[nr][nc] == true
+             || board[nr][nc] != word[idx+1]) continue;
             if(DFS(board,visited,word,idx+1,nr,nc) == true) return true;
         }
         visited[row][col] = false;
@@ -21,6 +20,7 @@ public:
     bool exist(vector<vector<char>>& board, string word) {
         m = board.size();
         n = board[0].size();
+        w = word.size();
         vector<vector<bool>> visited (m, vector<bool> (n,false));
         for(int i = 0; i<m; i++) {
             for(int j = 0; j<n; j++) {
