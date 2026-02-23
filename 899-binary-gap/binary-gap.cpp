3e9508c70801e0c5eaf269d1068cc5d38db046b2
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-02-23 11:29:28 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (94.71%) - LeetSync

--- 899-binary-gap/binary-gap.cpp
+++ 899-binary-gap/binary-gap.cpp
@@ -3,16 +3,17 @@ public:
     int binaryGap(int n) {
         vector<int> pos;
         int cnt = 0;
+        int prev_cnt = -1;
+        int res = 0;
         while(n>0) {
-            if(n%2 == 1) {
-                pos.push_back(cnt);
+            if(n%2 == 1 && prev_cnt == -1) {
+                prev_cnt = cnt;
+            } else if (n%2 == 1) {
+                res = max(res,cnt - prev_cnt);
+                prev_cnt = cnt;
             }
             cnt++;
             n /= 2;
-        }
-        int res = 0;
-        for(int i = 1; i<pos.size(); i++) {
-            res = max(res,pos[i] - pos[i-1]);
         }
         return res;
     }
