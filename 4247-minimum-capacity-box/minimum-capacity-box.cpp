9ea50f6e79cdb462a9c695062f3a788bcdb8a792
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-03-10 18:44:01 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (8.16%) - LeetSync

--- 4247-minimum-capacity-box/minimum-capacity-box.cpp
+++ 4247-minimum-capacity-box/minimum-capacity-box.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int minimumIndex(vector<int>& capacity, int itemSize) {
-        int res = 101;
         int idx = -1;
         for(int i = 0; i< capacity.size(); i++) {
-            if(capacity[i] >= itemSize && capacity[i] < res) {
-                res = capacity[i];
-                idx = i;
+            if(capacity[i] >= itemSize) {
+                if(idx == -1) idx = i;
+                else if (capacity[i] < capacity[idx]) {
+                    idx = i;
+                }
             }
         }
         return idx;
