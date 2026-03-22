cc96ece785d1c38ced70e2eb803877ea46f280bb
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-03-23 00:21:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (43.75%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -3,16 +3,15 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         if (n == 1) return nums[0];
-        if (n == 2) return max(nums[0],nums[1]);
-        vector<int> dp (n,-1);
+        vector<int> dp (n+1,-1);
         // dp[i] = max stolen money till ith house
-        dp[0] = nums[0];
-        dp[1] = max(nums[0],nums[1]);
-        for(int i = 2; i<n; i++) {
-            int steal = nums[i] + dp[i-2];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for(int i = 2; i<=n; i++) {
+            int steal = nums[i-1] + dp[i-2];
             int leave = dp[i-1];
             dp[i] = max(steal , leave);
         }
-        return dp[n-1];
+        return dp[n];
     }
 };
