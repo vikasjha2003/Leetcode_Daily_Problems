cf0395ea2331d74cf843f8115edb4b7a3002738a
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-03-23 00:23:06 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (60.84%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -5,6 +5,7 @@ public:
         if (n == 1) return nums[0];
         vector<int> dp (n+1,-1);
         // dp[i] = max stolen money till ith house
+        // creating house zero to make code modular, from house 2 onwards we will always have to check if picking current value is better or picking previous one is better.
         dp[0] = 0;
         dp[1] = nums[0];
         for(int i = 2; i<=n; i++) {
