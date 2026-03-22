8b909f5421eb7ded534b37140a9ade7f61579938
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-03-23 00:27:21 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (88.69%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -3,16 +3,14 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         if (n == 1) return nums[0];
-        vector<int> dp (n+1,-1);
-        // dp[i] = max stolen money till ith house
-        // creating house zero to make code modular, from house 2 onwards we will always have to check if picking current value is better or picking previous one is better.
-        dp[0] = 0;
-        dp[1] = nums[0];
-        for(int i = 2; i<=n; i++) {
-            int steal = nums[i-1] + dp[i-2];
-            int leave = dp[i-1];
-            dp[i] = max(steal , leave);
+        int prev2 = 0; // second previous house
+        int prev1 = nums[0]; // previous house
+        for(int i = 1; i<n; i++) {
+            int steal = nums[i] + prev2;
+            int leave = prev1;
+            prev2 = prev1;
+            prev1 = max(steal,leave);
         }
-        return dp[n];
+        return prev1;
     }
 };
