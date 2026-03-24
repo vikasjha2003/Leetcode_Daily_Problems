950b5378cff19f5c5e99b0cb63e2d4589dfad004
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-03-24 22:36:38 +0530

    Time: 123 ms (86.18%) | Memory: 94.9 MB (86.18%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -6,8 +6,8 @@ public:
         long long prev_even = 0, prev_odd = 0;
         long long cur_even = 0, cur_odd = 0;
         for(int i = 0; i < n; i++) {
-            cur_odd = max(prev_odd,prev_even + nums[i]);
-            cur_even = max(prev_even,prev_odd - nums[i]);
+            cur_odd = max(prev_odd,prev_even - nums[i]);
+            cur_even = max(prev_even,prev_odd + nums[i]);
             prev_odd = cur_odd;
             prev_even = cur_even;
         }
