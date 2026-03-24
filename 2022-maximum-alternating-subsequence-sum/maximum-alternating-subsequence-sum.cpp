f79ff5004f39d9ccd378516fdc29a986167eaf80
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-03-24 22:18:28 +0530

    Time: 257 ms (45.91%) | Memory: 157 MB (51.48%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int n;
-    long long solve (vector<int>& nums, int idx, bool flag,vector<vector<long long>> &dp) {
-        if(idx == n) return 0;
-        if(dp[idx][flag] != -1) return dp[idx][flag];
-        long long no_pick = solve(nums,idx + 1,flag,dp);
-        int val = nums[idx];
-        if(!flag) val = -val;
-        long long pick = solve(nums,idx + 1,!flag,dp) + val;
-        return dp[idx][flag] = max(pick,no_pick);
-    }
-
     long long maxAlternatingSum(vector<int>& nums) {
         n = nums.size();
-        vector<vector<long long>> dp (n,vector<long long> (2,-1));
-        return solve(nums,0,true,dp);
+        vector<vector<long long>> dp (n,vector<long long> (2,0));
+        dp[0][0] = nums[0];
+        for(int i = 1; i<n; i++) {
+            int val = nums[i];
+            for(int j = 0; j<2; j++) {
+                if(j == 1) val = -val;
+                long long skip = dp[i-1][j];
+                long long take = dp[i-1][!j] + val;
+                dp[i][j] = max(skip,take);
+            }
+        }
+        return max(dp[n-1][0],dp[n-1][1]);
     }
     
 };
