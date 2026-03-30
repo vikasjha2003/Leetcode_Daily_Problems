d62d7daf4c89b4c365721cd7b5c4c105662f23e7
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-03-30 12:15:35 +0530

    Time: 40 ms (29.89%) | Memory: 22.4 MB (28.16%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -8,25 +8,14 @@ public:
         for(int i = 0 ; i < n; i++) {
             if(i % 2 == 0) {
                 even[s1[i]]++;
+                even[s2[i]]--;
+                if(even[s2[i]] == 0) even.erase(s2[i]);
+                if(even[s1[i]] == 0) even.erase(s1[i]);
             } else {
                 odd[s1[i]]++;
-            }         
-        }
-        for(int i = 0 ; i < n; i++) {
-            if(i % 2 == 0) {
-                if(even.find(s2[i]) != even.end()) {
-                    even[s2[i]]--;
-                    if(even[s2[i]] == 0) even.erase(s2[i]);
-                } else {
-                    return false;
-                }
-            } else {
-                if(odd.find(s2[i]) != odd.end()) {
-                    odd[s2[i]]--;
-                    if(odd[s2[i]] == 0) odd.erase(s2[i]);
-                } else {
-                    return false;
-                }
+                odd[s2[i]]--;
+                if(odd[s2[i]] == 0) odd.erase(s2[i]);
+                if(odd[s1[i]] == 0) odd.erase(s1[i]);
             }         
         }
         if(even.empty() && odd.empty()) return true;
