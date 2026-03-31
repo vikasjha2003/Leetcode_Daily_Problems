c877219614b4659d8ef45daec2d7c0d696f1c8df
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-03-31 23:26:40 +0530

    Time: 692 ms (15.39%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -0,0 +1,5 @@
+select date_format(trans_date, '%Y-%m') as month , country, 
+count(id) as trans_count, sum(state = 'approved') as approved_count, 
+sum(amount) as trans_total_amount , sum((state = 'approved') * amount) as approved_total_amount
+from Transactions
+group by month , country;
