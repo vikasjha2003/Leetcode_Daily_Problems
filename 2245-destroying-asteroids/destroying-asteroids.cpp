06b0937b63e03c0503e81abb61c7ffac0d4b72fb
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-04-01 12:57:05 +0530

    Time: 40 ms (14.31%) | Memory: 106.9 MB (16.64%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.cpp
+++ 2245-destroying-asteroids/destroying-asteroids.cpp
@@ -4,12 +4,9 @@ public:
         int n = asteroids.size();
         sort(asteroids.begin(),asteroids.end());
         for(int i = 0; i<n; i++) {
-            if(mass > 1e5) return true;
-            if(asteroids[i] <= mass) {
-                mass += asteroids[i];
-            } else {
-                return false;
-            }
+            if(mass >= 1e5) return true;
+            else if(asteroids[i] <= mass) mass += asteroids[i];
+            else return false;
         }
         return true;
     }
