aeb57e3c20d579af77a75d1e1a1c2d367cd152c9
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-04-01 12:35:04 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (88.85%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -4,11 +4,9 @@ public:
         int n = asteroids.size();
         vector<int> res;
         for(int i = 0; i<n; i++) {
-            // -- , -+ , ++ allowed
             if(res.empty() || res.back() < 0 || asteroids[i] > 0) {
                 res.push_back(asteroids[i]);
-            } // +- collison
-            else {
+            } else {
                 if(res.back() == abs(asteroids[i])) {
                     res.pop_back();
                 } else if (res.back() < abs(asteroids[i])) {
