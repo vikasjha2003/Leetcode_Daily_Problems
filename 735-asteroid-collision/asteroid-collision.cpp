2d04806b86f05ea2d2df2814f01d3fa5f93e3efb
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-04-01 12:34:34 +0530

    Time: 2 ms (33.21%) | Memory: 21.7 MB (77.54%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -2,30 +2,17 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         int n = asteroids.size();
-        vector<pair<int,int>> v;
-        // 0 -> negative, 1 -> positive
-        for(int i = 0; i<n; i++) {
-            if(asteroids[i] > 0) {
-                v.push_back({asteroids[i],1});
-            } else {
-                v.push_back({asteroids[i],0});
-            }
-        }
         vector<int> res;
-        stack<int> st; 
         for(int i = 0; i<n; i++) {
-            // 00 , 01 , 11 allowed
-            if(st.empty() || st.top() == 0 || st.top() == v[i].second) {
-                res.push_back(v[i].first);
-                st.push(v[i].second);
-            } // 10 collison
+            // -- , -+ , ++ allowed
+            if(res.empty() || res.back() < 0 || asteroids[i] > 0) {
+                res.push_back(asteroids[i]);
+            } // +- collison
             else {
-                if(abs(res.back()) == abs(v[i].first)) {
+                if(res.back() == abs(asteroids[i])) {
                     res.pop_back();
-                    st.pop();
-                } else if (abs(res.back()) < abs(v[i].first)) {
+                } else if (res.back() < abs(asteroids[i])) {
                     res.pop_back();
-                    st.pop();
                     i--;
                 }
             }
