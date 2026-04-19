80a254ffb2b805c3d8906811f0c15d218a86b287
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-04-19 18:24:37 +0530

    Time: 642 ms (21.37%) | Memory: 339.8 MB (20.68%) - LeetSync

--- 4283-multi-source-flood-fill/multi-source-flood-fill.cpp
+++ 4283-multi-source-flood-fill/multi-source-flood-fill.cpp
@@ -2,35 +2,32 @@ class Solution {
 public:
     vector<vector<int>> colorGrid(int n, int m, vector<vector<int>>& sources) {
         vector<vector<int>> grid (n, vector<int> (m,0));
-        vector<vector<int>> visited (n, vector<int> (m,0));
         sort(sources.begin(), sources.end(), [] (auto &v1, auto &v2) {
             return v1[2] > v2[2];
         });
 
         queue<vector<int>> q;
-        for(int i = 0; i<sources.size(); i++) {
-            visited[sources[i][0]][sources[i][1]] = 1;
-            q.push(sources[i]);
+        for(auto &s : sources) {
+            grid[s[0]][s[1]] = s[2];
+            q.push(s);
         }
 
         int drow[4] = {1,0,-1,0};
         int dcol[4] = {0,1,0,-1};
 
         while(!q.empty()) {
-            int size = q.size();
-            while(size--) {
-                auto it = q.front();
-                q.pop();
-                
-                grid[it[0]][it[1]] = max(grid[it[0]][it[1]],it[2]);
+            auto it = q.front();
+            q.pop();
 
-                for(int i = 0; i<4; i++) {
-                    int row = it[0] + drow[i];
-                    int col = it[1] + dcol[i];
-                    if(row < 0 || row == n || col < 0 || col == m || visited[row][col]) continue;
-                    visited[row][col] = 1;
-                    q.push({row,col,it[2]});
-                }
+            for(int i = 0; i<4; i++) {
+                int row = it[0] + drow[i];
+                int col = it[1] + dcol[i];
+
+                if(row < 0 || row == n || col < 0 || col == m || grid[row][col]) continue;
+
+                grid[row][col] = it[2];
+
+                q.push({row,col,it[2]});
             }
         }
         
