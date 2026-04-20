e6220c001bfd7a3877b9e4f35aaca8dbe1252036
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-04-20 12:19:37 +0530

    Time: 196 ms (15.58%) | Memory: 13.5 MB (31.91%) - LeetSync

--- 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
+++ 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -7,7 +7,7 @@ public:
             if(cost == K) return 1;
             return 0;
         }
-        if(dp[maxi][size][cost] != -1) return dp[maxi][size][cost];
+        if(dp[size][cost][maxi] != -1) return dp[size][cost][maxi];
         int res = 0;
         for(int i = 1; i <= M; i++) {
             if(i > maxi) {
@@ -16,12 +16,12 @@ public:
                 res = (res + solve(maxi, size+1, cost, dp)) % mod;
             }
         }
-        return dp[maxi][size][cost] = res;
+        return dp[size][cost][maxi] = res;
     }
 
     int numOfArrays(int n, int m, int k) {
         N = n ,M = m, K = k;
-        vector<vector<vector<int>>> dp (m+1,vector<vector<int>> (n+1, vector<int> (k+1,-1)));
+        vector<vector<vector<int>>> dp (n+1,vector<vector<int>> (k+1, vector<int> (m+1,-1)));
         return solve(0,0,0,dp);
     }
 };
