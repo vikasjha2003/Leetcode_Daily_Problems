1ae20147b76724e53d629806424dc37f4a494226
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-04-20 18:19:32 +0530

    Time: 3 ms (55.15%) | Memory: 28.2 MB (80.29%) - LeetSync

--- 4252-first-unique-even-element/first-unique-even-element.cpp
+++ 4252-first-unique-even-element/first-unique-even-element.cpp
@@ -3,7 +3,7 @@ public:
     int firstUniqueEven(vector<int>& nums) {
         int n = nums.size();
         vector<int> arr (101,0);
-        for(int i = n-1; i >= 0; i--) {
+        for(int i = 0; i < n; i++) {
             if(nums[i] % 2 == 0) {
                 arr[nums[i]]++;
             }
