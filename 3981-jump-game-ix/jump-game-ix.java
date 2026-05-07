580468fa9dcea0292f826d8bf3dc3af1bd586f9b
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-05-07 10:58:49 +0530

    Time: 4 ms (100.00%) | Memory: 203.1 MB (11.29%) - LeetSync

--- 3981-jump-game-ix/jump-game-ix.java
+++ 3981-jump-game-ix/jump-game-ix.java
@@ -3,25 +3,24 @@ class Solution {
     public int[] maxValue(int[] nums) {
         int n = nums.length;
 
-        int[] pmax = new int[n];
-        pmax[0] = nums[0];
+        int[] ans = new int[n];
+        ans[0] = nums[0];
+
+        // calculate prefixmax
 
         for(int i = 1; i<n; i++) {
-            pmax[i] = Math.max(pmax[i-1],nums[i]);
+            ans[i] = Math.max(ans[i-1],nums[i]);
         }
 
-        int ans[] = new int[n];
-        ans[n-1] = pmax[n-1];
         int mini = nums[n-1];
 
         for(int i = n-2; i>= 0; i--) {
-            if(pmax[i] > mini) {
+            if(ans[i] > mini) {
                 ans[i] = ans[i+1];
-            } else {
-                ans[i] = pmax[i];
             }
             mini = Math.min(mini,nums[i]);
         }
+        
         return ans;
     }
 }
