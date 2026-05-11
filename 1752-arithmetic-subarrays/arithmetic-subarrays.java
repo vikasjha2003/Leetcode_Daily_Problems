e1f8812eae37b916e22d668f7cd4b291dbbe5040
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-05-11 12:14:59 +0530

    Time: 22 ms (84.13%) | Memory: 53.4 MB (6.59%) - LeetSync

--- 1752-arithmetic-subarrays/arithmetic-subarrays.java
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.java
@@ -1,10 +1,19 @@
 class Solution {
     public int[] copyOf (int[] nums, int l, int r) {
         int[] arr = new int[r-l+1];
-        for(int i = 0; i <= r - l; i++) {
+        for(int i = 0; i < arr.length; i++) {
             arr[i] = nums[i+l];
         }
         return arr;
+    }
+    public Boolean flag (int[] arr) {
+        int x = arr[1] - arr[0];
+        for(int j = 2; j<arr.length; j++) {
+            if(arr[j] - arr[j-1] != x) {
+                return false;
+            }
+        }
+        return true;
     }
     public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
         int m = l.length;
@@ -15,15 +24,7 @@ class Solution {
         for(int i = 0; i<m; i++) {
             int[] arr = copyOf(nums,l[i],r[i]);
             Arrays.sort(arr);
-            Boolean flag = true;
-            int x = arr[1] - arr[0];
-            for(int j = 2; j<arr.length; j++) {
-                if(arr[j] - arr[j-1] != x) {
-                    flag = false;
-                    break;
-                }
-            }
-            ans.add( flag);
+            ans.add(flag(arr));
         }
 
         return ans;
