dc47e9822d1bb9e2ac7c3f1180af22b3d5e26797
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-05-11 12:43:41 +0530

    Time: 33 ms (27.25%) | Memory: 47 MB (87.72%) - LeetSync

--- 1752-arithmetic-subarrays/arithmetic-subarrays.java
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.java
@@ -1,4 +1,5 @@
 class Solution {
+
     public int[] copyOf (int[] nums, int l, int r) {
         int[] arr = new int[r-l+1];
         for(int i = 0; i < arr.length; i++) {
@@ -6,27 +7,41 @@ class Solution {
         }
         return arr;
     }
+
     public Boolean flag (int[] arr) {
-        int x = arr[1] - arr[0];
-        for(int j = 2; j<arr.length; j++) {
-            if(arr[j] - arr[j-1] != x) {
-                return false;
-            }
+        int maxi = arr[0];
+        int mini = arr[0];
+        Set<Integer> st = new HashSet();
+        st.add(arr[0]);
+
+        for(int i = 1; i<arr.length; i++) {
+            maxi = Math.max(maxi,arr[i]);
+            mini = Math.min(mini,arr[i]);
+            st.add(arr[i]);
         }
+
+        if((maxi - mini) % (arr.length - 1) != 0) return false;
+
+        int d = (maxi - mini) / (arr.length - 1);
+        int a = mini + d;
+
+        while(a < maxi) {
+            if(!st.contains(a)) return false;
+            a += d;
+        }
+
         return true;
     }
-    public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
-        int m = l.length;
-        int n = nums.length;
 
+    public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
         List<Boolean> ans = new ArrayList<>();
 
-        for(int i = 0; i<m; i++) {
+        for(int i = 0; i<l.length; i++) {
             int[] arr = copyOf(nums,l[i],r[i]);
-            Arrays.sort(arr);
             ans.add(flag(arr));
         }
 
         return ans;
     }
+
 }
