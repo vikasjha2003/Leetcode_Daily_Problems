ac2ad887b8cd658d602773c42e77a11f48c2f032
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-05-15 02:44:54 +0530

    Time: 9 ms (21.53%) | Memory: 116.2 MB (11.11%) - LeetSync

---
+++ 2465-shifting-letters-ii/shifting-letters-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String shiftingLetters(String s, int[][] shifts) {
+        StringBuilder sb = new StringBuilder(s);
+        int[] diff = new int[s.length()];
+
+        for(int[] q : shifts) {
+            int a = 1;
+            if(q[2] == 0) a = -1;
+            diff[q[0]] += a;
+            if(q[1] + 1 < diff.length) diff[q[1] + 1] -= a; 
+        }
+
+        int cumsum = 0;
+
+        for(int i = 0; i<diff.length; i++) {
+            cumsum += diff[i];
+            int a = cumsum + (sb.charAt(i) - 'a');
+            int mod = Math.floorMod(a, 26);
+            sb.setCharAt(i,(char)(mod + 'a'));
+        }
+
+        return sb.toString();
+    }
+}
