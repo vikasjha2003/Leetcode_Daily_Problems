4221e698a5b6a3854f404ee6dc1ac103834c9271
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-05-15 12:51:16 +0530

    Time: 27 ms (74.10%) | Memory: 212.7 MB (90.04%) - LeetSync

---
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
@@ -0,0 +1,45 @@
+class Solution {
+    private boolean isZero(int[] nums, int[][] queries, int mid) {
+        int[] diff = new int[nums.length];
+
+        for(int i = 0; i <= mid; i++) {
+            int l = queries[i][0];
+            int r = queries[i][1];
+            int val = queries[i][2];
+            diff[l] -= val;
+            if(r + 1 < nums.length) diff[r+1] += val;
+        }
+
+        int cumsum = 0;
+
+        for(int i = 0; i<nums.length; i++) {
+            cumsum += diff[i];
+            if(nums[i] + cumsum > 0) return false;
+        }
+
+        return true;
+    }
+    public boolean check (int[] nums, int [][] queries) {
+        for(int i = 0; i<nums.length; i++) {
+            if(nums[i] != 0) return false;
+        }
+        return true;
+    }
+    public int minZeroArray(int[] nums, int[][] queries) {
+        if(check(nums,queries)) return 0;
+        int low = 0;
+        int high = queries.length - 1;
+        int ans = Integer.MAX_VALUE;
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            if(isZero(nums,queries,mid)) {
+                ans = Math.min(ans,mid+1);
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+}
