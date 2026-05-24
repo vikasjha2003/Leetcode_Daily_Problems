16a3def30e0d2a1b7b63eb0ee3284cf09aee5a44
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-05-24 23:54:54 +0530

    Time: 10 ms (78.29%) | Memory: 19 MB (56.42%) - LeetSync

---
+++ 1466-jump-game-v/jump-game-v.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int n;
+
+    int solve (vector<int>& arr, int d, int idx, vector<int> &dp) {
+        if(dp[idx] != 0) return dp[idx];
+
+        int cnt = 0;
+
+        for(int i = 1; i<= d; i++) {
+            if(idx + i < n && arr[idx + i] < arr[idx]) {
+                cnt = max(cnt,solve(arr,d,idx+i,dp));
+            } else break;
+        }
+
+        for(int i = 1; i<= d; i++) {
+            if(idx - i >= 0 && arr[idx - i] < arr[idx]) {
+                cnt = max(cnt,solve(arr,d,idx-i,dp));
+            } else break;
+        }
+
+        return dp[idx] = cnt + 1;
+    }
+
+    int maxJumps(vector<int>& arr, int d) {
+        n = arr.size();
+
+        vector<int> dp (n,0);
+
+        int res = 0;
+
+        for(int i = 0; i<n; i++) {
+            res = max(res,solve(arr,d,i,dp)); 
+        }
+
+        return res;
+    }
+};
