316174950068bcdef6cb25f41c57ae9e676891ce
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-05-31 10:07:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (96.19%) - LeetSync

---
+++ 4321-digit-frequency-score/digit-frequency-score.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int digitFrequencyScore(int n) {
+        int res = 0;
+
+        while(n > 0) {
+            res += n%10;
+            n /= 10;
+        }
+
+        return res;
+    }
+};
